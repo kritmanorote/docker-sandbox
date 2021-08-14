e1e61b2cc2834312c91d7a7a50ecaefd42862780
Kritakorn Kritmanorote <kkritmanorote@dstworldwideservices.com>
2021-08-14 22:27:25 +0700

    First commit

---
+++ src/main/java/com/chill/dockersandbox/DockerSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.chill.dockersandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerSandboxApplication.class, args);
+	}
+
+}
